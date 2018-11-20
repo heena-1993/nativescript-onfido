36dbe6876f74ebda2101cb358bbceda81f62ca62
Michael Wilson <michael@Michaels-MacBook-Pro.local>
2018-11-20 16:29:26 +0000

    added onfido

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,14 +1,9 @@
 import { Observable } from 'tns-core-modules/data/observable';
-import { Onfido } from 'nativescript-onfido';
 
 export class HelloWorldModel extends Observable {
   public message: string;
-  private onfido: Onfido;
 
   constructor() {
     super();
-
-    this.onfido = new Onfido();
-    this.message = this.onfido.message;
   }
 }
